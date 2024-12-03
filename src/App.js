cb060f950b80b7571ca0e183d303c0e853506acc
Vishal Rajput <99860031+rpVishal@users.noreply.github.com>
2024-12-04 01:55:26 +0530

    Update App.js
    Removed extra spaces

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
-// src/App.js
-import React, { useState } from 'react';
 import Login from './Login';
 import Welcome from './Welcome';
 import './index.css';
 
 const App = () => {
   const [user, setUser] = useState(null);
-
+  
   const handleLogout = () => {
     setUser(null);
   };
-
+  
   return (
     <div className="app-container">
       {!user ? (
