7f70d821c28d18a4bb71c6d2ab2f973830b88de6
Vishal Rajput <99860031+rpVishal@users.noreply.github.com>
2024-12-04 01:54:55 +0530

    Update firebase-config.js
    Removed personal tokens and keys

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -2,13 +2,13 @@ import { initializeApp } from 'firebase/app';
 import { getAuth, GoogleAuthProvider } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD0v0NByTpQqZE4noBe6tA6M0YRrddl9oE",
-  authDomain: "coding-assignment-920fc.firebaseapp.com",
-  projectId: "coding-assignment-920fc",
-  storageBucket: "coding-assignment-920fc.firebasestorage.app",
-  messagingSenderId: "679152655064",
-  appId: "1:679152655064:web:796a21889fd2b695e615ad",
-  measurementId: "G-SPZB0B4XF5"
+  apiKey: "Paste your config",
+  authDomain: "Paste your config",
+  projectId: "Paste your config",
+  storageBucket: "Paste your config",
+  messagingSenderId: "Paste your config",
+  appId: "Paste your config",
+  measurementId: "Paste your config"
 };
 
 const app = initializeApp(firebaseConfig);
